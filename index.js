fb5fbb7060319859a86591e835547af3aaf95776
Josh Bowden <git@josh.tf>
2020-03-07 00:44:44 +1100

    add project base

---
+++ index.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", msg => {
+  if (msg.substring(0, 1) == ">") {
+    var args = msg.substring(1).split(" ");
+    var cmd = args[0];
+
+    args = args.splice(1);
+    switch (cmd) {
+      case "owo":
+        msg.reply("uwu");
+        break;
+    }
+  }
+});
+
+client.login("token");
