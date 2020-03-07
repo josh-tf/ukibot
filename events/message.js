2f2476896cfc581ba24282b63be0fdae223ddfc2
Josh Bowden <git@josh.tf>
2020-03-07 13:14:13 +1100

    add beep command

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 const owo = require('../commands/owo')
 const oof = require('../commands/oof')
+const beep = require('../commands/beep')
 
 module.exports = (client, message) => {
   switch(message.content) {
@@ -8,6 +9,9 @@ module.exports = (client, message) => {
       break;
     case 'oof':
       return oof(message)
+      break;
+    case 'beep':
+      return beep(message)
       break;
   }
 
