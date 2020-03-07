519e4d3efda305bdbd9aa238968e13bbfd82af58
Josh Bowden <git@josh.tf>
2020-03-07 14:35:56 +1100

    start adding music feature

--- events/message.js
+++ events/message.js
@@ -1,18 +1,34 @@
-const owo = require('../commands/owo')
-const oof = require('../commands/oof')
-const beep = require('../commands/beep')
+const owo = require("../commands/owo");
+const oof = require("../commands/oof");
+const beep = require("../commands/beep");
 
 module.exports = (client, message) => {
-  switch(message.content) {
-    case 'owo':
-      return owo(message)
-      break;
-    case 'oof':
-      return oof(message)
-      break;
-    case 'beep':
-      return beep(message)
-      break;
-  }
+  // if the message is from the bot then do nothing
+  if (message.author.bot) return;
 
-}
+  // ignore messages without the prefix
+  if (!message.content.startsWith(prefix)) return;
+
+  //! handle our commands, need to clean this up later
+
+  const serverQueue = queue.get(message.guild.id);
+
+  if (message.content.startsWith(`${prefix}play`)) {
+    execute(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}skip`)) {
+    skip(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}stop`)) {
+    stop(message, serverQueue);
+    return;
+  } else if (message.content.startsWith(`${prefix}beep`)) {
+    return beep(message);
+  } else if (message.content.startsWith(`${prefix}oof`)) {
+    return oof(message);
+  } else if (message.content.startsWith(`${prefix}owo`)) {
+    return owo(message);
+  } else {
+    message.channel.send("You did not enter a valid command 😢");
+  }
+};
