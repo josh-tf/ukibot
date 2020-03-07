ec169c532589d303fc1447ee946d5b50b7e2e2a3
Restyled.io <commits@restyled.io>
2020-03-07 23:58:20 +0000

    Restyled by clang-format

--- commands/oof.js
+++ commands/oof.js
@@ -1,9 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports = {
-  name: "oof",
-  description: "Reply with oof in all caps",
-  execute(message) {
-    message.channel.send(`OOF !`);
-  }
+  name : "oof",
+  description : "Reply with oof in all caps",
+  execute(message) { message.channel.send(`OOF !`); }
 };
--- commands/purge.js
+++ commands/purge.js
@@ -1,6 +1,6 @@
 module.exports = {
-  name: "purge",
-  description: "Delete the last messages in all chats.",
+  name : "purge",
+  description : "Delete the last messages in all chats.",
   async execute(message) {
     if (!message.member.hasPermission("ADMINISTRATOR")) {
       return message.reply("You need to be an admin to run this command");
@@ -12,22 +12,16 @@ module.exports = {
       deleteCount = parseInt(args[1], 10);
     } catch (err) {
       return message.reply(
-        "Please provide the number of messages to delete. (max 100)"
-      );
+          "Please provide the number of messages to delete. (max 100)");
     }
 
     if (!deleteCount || deleteCount < 2 || deleteCount > 100)
       return message.reply(
-        "Please provide a number between 2 and 100 for the number of messages to delete"
-      );
+          "Please provide a number between 2 and 100 for the number of messages to delete");
 
-    const fetched = await message.channel.fetchMessages({
-      limit: deleteCount
-    });
-    message.channel
-      .bulkDelete(fetched)
-      .catch(error =>
-        message.reply(`Couldn't delete messages because of: ${error}`)
-      );
+    const fetched = await message.channel.fetchMessages({limit : deleteCount});
+    message.channel.bulkDelete(fetched).catch(
+        error =>
+            message.reply(`Couldn't delete messages because of: ${error}`));
   }
 };
