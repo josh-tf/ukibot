2a48ec46c0f20bbc5bb3ac2c2e1ca3187bee715d
Restyled.io <commits@restyled.io>
2020-03-08 00:27:39 +0000

    Restyled by prettier

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -1,20 +1,20 @@
 const Discord = require("discord.js");
 
 function formatDate(date) {
-  var d = new Date(date), month = "" + (d.getMonth() + 1),
-      day = "" + d.getDate(), year = d.getFullYear();
+  var d = new Date(date),
+    month = "" + (d.getMonth() + 1),
+    day = "" + d.getDate(),
+    year = d.getFullYear();
 
-  if (month.length < 2)
-    month = "0" + month;
-  if (day.length < 2)
-    day = "0" + day;
+  if (month.length < 2) month = "0" + month;
+  if (day.length < 2) day = "0" + day;
 
-  return [ day, month, year ].join("/");
+  return [day, month, year].join("/");
 }
 
 module.exports = {
-  name : "userinfo",
-  description : "Get information about a user.",
+  name: "userinfo",
+  description: "Get information about a user.",
   execute(message) {
     const member = message.mentions.members.first();
     const user = member.user;
@@ -23,33 +23,34 @@ module.exports = {
     const createdAt = formatDate(user.createdAt);
 
     const embed = {
-      title : `${user.username}`,
-      color : 15072487,
-      timestamp : "2020-03-07T23:38:01.994Z",
-      footer : {
-        icon_url :
-            "https://cdn.discordapp.com/avatars/617520090535165952/fc142a26ffbe22234d41df600553329b.png",
-        text : "ukibot.js lookup"
+      title: `${user.username}`,
+      color: 15072487,
+      timestamp: "2020-03-07T23:38:01.994Z",
+      footer: {
+        icon_url:
+          "https://cdn.discordapp.com/avatars/617520090535165952/fc142a26ffbe22234d41df600553329b.png",
+        text: "ukibot.js lookup"
       },
-      thumbnail : {url : `${user.avatarURL}`},
-      author : {
-        name : "ukibot v1.0.0",
-        url : "https://discordapp.com",
-        icon_url :
-            "https://cdn.discordapp.com/avatars/617520090535165952/fc142a26ffbe22234d41df600553329b.png"
+      thumbnail: { url: `${user.avatarURL}` },
+      author: {
+        name: "ukibot v1.0.0",
+        url: "https://discordapp.com",
+        icon_url:
+          "https://cdn.discordapp.com/avatars/617520090535165952/fc142a26ffbe22234d41df600553329b.png"
       },
-      fields : [
-        {name : "User ID", value : `${user.id}`, inline : true},
-        {name : "Discord ID", value : `${user.tag}`, inline : true},
-        {name : "Joined Hangout", value : `${joinedAt}`, inline : true},
-        {name : "Joined Discord", value : `${createdAt}`, inline : true}, {
-          name : "Current Status",
-          value : `${user.presence.status}`,
-          inline : true
+      fields: [
+        { name: "User ID", value: `${user.id}`, inline: true },
+        { name: "Discord ID", value: `${user.tag}`, inline: true },
+        { name: "Joined Hangout", value: `${joinedAt}`, inline: true },
+        { name: "Joined Discord", value: `${createdAt}`, inline: true },
+        {
+          name: "Current Status",
+          value: `${user.presence.status}`,
+          inline: true
         },
-        {name : "Role", value : `${member.roles.name}`, inline : true}
+        { name: "Role", value: `${member.roles.name}`, inline: true }
       ]
     };
-    message.channel.send({embed});
+    message.channel.send({ embed });
   }
 };
